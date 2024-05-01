948db21f53c40836422395eca0be4799b4da7708
ChandraShekhar <csyadav0513@gmail.com>
2024-05-01 12:15:31 +0530

    Add Next.js Image component and update home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
+import Image from "next/image";
 
-const page = () => {
+const Home = () => {
   return (
-    <div>page</div>
-  )
-}
-export default page
+    <>
+      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flx flex-col justify-center">
+            <p className="small-text">
+              Smart Shopping Star Here:
+              <Image
+                src={"/assets/icons/arrow-right.svg"}
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className="head-text">
+              Unleash the Power of{" "}
+              <span className="text-primary"> PriceWise</span>
+            </h1>
+            <p className="mt-6">
+              Great deals, price comparison, and reviews to help you make the
+              right choice.
+              <br />
+            </p>
+            Searchbar
+          </div>
+          HeroCarousel
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {}
+        </div>
+      </section>
+    </>
+  );
+};
+export default Home;
