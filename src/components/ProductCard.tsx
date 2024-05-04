698012592d3193160a6557a62666882c399d36ca
ChandraShekhar <csyadav0513@gmail.com>
2024-05-04 18:08:08 +0530

    Improve ProductCard component with lazy loading for images

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -9,14 +9,33 @@ interface ProductProps {
 const ProductCard = ({ product }: ProductProps) => {
   return (
     <>
-      <Link href={`/products/${product._id}`}>
-        <div>
+      <Link
+        href={`/products/${product._id}`}
+        className="block w-full md:w-72 rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 mx-auto border border-gray-200 hover:border-gray-400"
+      >
+        <div className="relative h-40 md:h-52 overflow-hidden">
           <Image
             src={product.image}
             alt={product.title}
-            width={200}
-            height={200}
+            layout="fill"
+            objectFit="cover"
+            className="cursor-pointer hover:scale-105 transform transition-transform duration-300"
           />
+        </div>
+
+        <div className="p-4">
+          <h3 className="text-lg font-medium  mb-2 truncate hover:text-gray-600 ">
+            {product.title}
+          </h3>
+
+          <div className="flex justify-between items-center">
+            <p className="text-sm ">{product.category}</p>
+
+            <p className="text-lg ">
+              <span className="mr-1">{product.currency}</span>
+              <span>{product.currentPrice}</span>
+            </p>
+          </div>
         </div>
       </Link>
     </>
