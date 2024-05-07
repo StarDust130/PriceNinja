4cfd9dafa56f99537040588182434db3d166fcc8
ChandraShekhar <csyadav0513@gmail.com>
2024-05-07 20:46:15 +0530

    Improve Ui of Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,31 +11,43 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import Link from "next/link";
+import { Input } from "./ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "./ui/button";
 
 const Modal = () => {
   return (
     <>
-    <AlertDialog>
-  <AlertDialogTrigger>Open</AlertDialogTrigger>
-  <AlertDialogContent>
-    <AlertDialogHeader>
-      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-      <AlertDialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </AlertDialogDescription>
-    </AlertDialogHeader>
-    <AlertDialogFooter>
-      <AlertDialogCancel>Cancel</AlertDialogCancel>
-      <AlertDialogAction>Continue</AlertDialogAction>
-    </AlertDialogFooter>
-  </AlertDialogContent>
-</AlertDialog>
-
-    
-      
-
+      <AlertDialog>
+        <AlertDialogTrigger>Open</AlertDialogTrigger>
+        <AlertDialogContent>
+          <div className="flex  items-center space-x-2">
+            <Link className="text-2xl font-bold " href="/">
+              Price<span className="text-red-500">Ninja</span>
+            </Link>
+          </div>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Stay Updated with product pricing alerts right in your inbox!
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              Never miss a deal, subscribe to our newsletter now.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogDescription>
+            <Label htmlFor="email">Email address</Label>
+            <Input type="email" id="email" placeholder="Email" />
+          </AlertDialogDescription>
+          <AlertDialogFooter>
+            {/* <AlertDialogCancel>Cancel</AlertDialogCancel> */}
+            <AlertDialogAction>
+              <Button>Track Product</Button>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
-  )
-}
-export default Modal
+  );
+};
+export default Modal;
